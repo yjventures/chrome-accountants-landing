7d3a21f519b7f6ccb562ccf3655f9d25f0a3bc3b
yjventures <youssef@getactively.app>
2025-09-15 12:11:59 +0600

    Pagespeed Mobile Update 3

--- vite.config.ts
+++ vite.config.ts
@@ -22,27 +22,11 @@ export default defineConfig({
     // Performance optimizations
     target: 'es2015',
     minify: 'esbuild',
-    cssCodeSplit: true,
-    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom', 'react-router-hash-link'],
-          ui: ['@radix-ui/react-accordion', '@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu'],
-          icons: ['lucide-react'],
-          forms: ['react-hook-form', '@hookform/resolvers', 'zod'],
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
-            return `assets/fonts/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
         },
       },
     },
